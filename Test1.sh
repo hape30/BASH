aef67477dc0a157e667a50a00d0884c5ad4f3773
hape30 <87194171+hape30@users.noreply.github.com>
2023-05-15 01:01:15 +0300

    Update Test1.sh

--- Test1.sh
+++ Test1.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 chmod +x Test1.sh
 ./script17.sh 
-echo "проверка 1 теста...."
+  echo "проверка 1 теста...."
 if [[ $? -eq 1 ]]
 then 
-echo "1 тест прошел успешно"
+  echo "1 тест прошел успешно"
 else
-echo "1 тест не пройдет"
-exit 1 
+  echo "1 тест не пройдет"
+   exit 1 
 fi
-echo "проверка 2 теста...."
+  echo "проверка 2 теста...."
 ./script17.sh inputafile outputafile word zero 
 if [[ $? -eq 1 ]]
 then 
-echo "2 тест прошел успешно"
+  echo "2 тест прошел успешно"
 else 
-echo "2 тест не пройден"
-exit 1 
+  echo "2 тест не пройден"
+  exit 1 
 fi
-echo "проверка 3 теста...."
+  echo "проверка 3 теста...."
 mkdir test
 ./script17.sh test outputafile Мастер
 if [[ $? -eq 1 ]]
 then 
-echo "3 тест прошел успешно"
+  echo "3 тест прошел успешно"
 else 
-echo "3 тест не пройден"
-exit 1 
+  echo "3 тест не пройден"
+  exit 1 
 fi
 rmdir test
 
