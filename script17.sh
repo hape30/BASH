80dd82d266253ee2223d675fe8db49ca2e19afcc
hape30 <sasha2007.ivanov@mail.ru>
2023-05-23 13:18:45 +0300

    initial commit

--- script17.sh
+++ script17.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
+read -p " Введите искомое значение:" input
+read -p " Введите искомое значение:" output
 
-if [[ $# -lt 3 ]]
+if [[ $# -lt 1 ]]
 then
     echo "Мало аргумнтов "
     exit 1
-elif [[ $# -gt 3 ]]
+elif [[ $# -gt 1 ]]
 then
     echo "Много аргументов"
     exit 1
 fi
 
-if [[ ! -f $1 || ! -f $2 ]]
+if [[ ! -f $input ]]
 then
-    echo " $1 || $2 не являтся файлами"
+    echo " $input  не являтся файлом"
     exit 1
 fi
-input_file=$1
-output_file=$2
-search_word=$3
+if [[ ! -f $output ]]
+then
+    echo " $output  не являтся файлом"
+    exit 1
+fi
+input_file=$input
+output_file=$output
+search_word=$1
 
-grep -o  "$search_word" "$input_file" | wc -l > "$output_file"
+grep -o  "$search_word" "$input" | wc -l > $output
 #grep -o "$search_word" "$input_file" — команда grep ищет в файле $input_file все вхождения слова, которое содержится в переменной $search_word. Опция -o указывает на вывод только самих найденных слов, без контекста.
-
+echo "кол-во слов  в списке" >> $output_file
 
 echo "Программа выполнена успешно"
